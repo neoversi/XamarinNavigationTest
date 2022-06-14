6b4e65e5e02a248dc834b2cf26617970042cd23c
Hamdy Ali <hamdy.aliman.ali@gmail.com>
2022-06-14 08:22:37 +0800

    Initial Commit

---
+++ XamarinNavigationTest/ViewModel/MainVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XamarinNavigationTest.Views;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XamarinNavigationTest.ViewModel
+{
+    class MainVM
+    {
+        public MainVM()
+        {
+            ToSecondPage = new Command(ToSecondPageClicekd);
+
+        }
+
+        public Command ToSecondPage { get; set; }
+        public async void ToSecondPageClicekd()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
---
+++ XamarinNavigationTest/ViewModel/SecondVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XamarinNavigationTest.Views;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XamarinNavigationTest.ViewModel
+{
+    class SecondVM
+    {
+        public SecondVM()
+        {
+            ToThirdPage = new Command(ToThirdPageClicekd);
+        }
+        public Command ToThirdPage { get; set; }
+        public async void ToThirdPageClicekd()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new ThirdPage());
+        }
+    }
+}
