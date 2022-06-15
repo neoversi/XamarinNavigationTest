d676450a940a9bed17f03f10f4bfdc35e51f9e2e
Hamdy Ali <hamdy.aliman.ali@gmail.com>
2022-06-15 10:59:57 +0800

    Add syncfusion button nuget and comment out regular buttons with syncfusion buttons

--- XamarinNavigationTest/App.xaml.cs
+++ XamarinNavigationTest/App.xaml.cs
@@ -10,7 +10,7 @@ namespace XamarinNavigationTest
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
